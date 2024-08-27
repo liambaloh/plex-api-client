2b682b7149694ddd4a1af9ce1280db4424426a00
LiamLime <contact@liamlime.com>
2024-08-27 20:49:57 +0100

    - Adds ability to control which types of libraries get printed

--- src/main/kotlin/com/limalime/plexapi/Main.kt
+++ src/main/kotlin/com/limalime/plexapi/Main.kt
@@ -20,37 +20,47 @@ fun main() {
             println("$index -> ${directory.title} (Key: ${directory.key}, type: ${directory.type})")
         }
 
+        val printFilms = false
+        val printShows = false
+        val printMusic = false
+
         libraries.directory.forEachIndexed { libraryIndex, libraryDirectory ->
             println("# ${libraryDirectory.title}")
             when (libraryDirectory.type) {
                 "movie" -> {
-                    println("## Films")
-                    val series = plexApi.getFilms(libraryDirectory.key!!.toInt())
-                    series.videos.forEachIndexed { videoIndex, videoDirectory ->
-                        println(" | Film $videoIndex -> ${videoDirectory.title} (Key: ${videoDirectory.ratingKey})")
-                        val metadata = plexApi.getFilmMetadata(videoDirectory.ratingKey!!.toInt())
-                        println(" | |-> ${metadata.videos?.firstOrNull()?.summary}")
+                    if (printFilms) {
+                        println("## Films")
+                        val series = plexApi.getFilms(libraryDirectory.key!!.toInt())
+                        series.videos.forEachIndexed { videoIndex, videoDirectory ->
+                            println(" | Film $videoIndex -> ${videoDirectory.title} (Key: ${videoDirectory.ratingKey})")
+                            val metadata = plexApi.getFilmMetadata(videoDirectory.ratingKey!!.toInt())
+                            println(" | |-> ${metadata.videos?.firstOrNull()?.summary}")
+                        }
                     }
                 }
 
                 "show" -> {
-                    println("## Series")
-                    val series = plexApi.getSeries(libraryDirectory.key!!.toInt())
-                    series.directory.forEachIndexed { seriesIndex, seriesDirectory ->
-                        println(" | Series $seriesIndex -> ${seriesDirectory.title} (Key: ${seriesDirectory.ratingKey})")
-                        val seriesMetadata = plexApi.getSeriesMetadata(seriesDirectory.ratingKey!!.toInt())
-                        println(" | * ${seriesMetadata.directories?.firstOrNull()?.summary}")
-                        val seasonsMetadata = plexApi.getSeriesSeasons(seriesDirectory.ratingKey!!.toInt())
-                        seasonsMetadata.directories?.forEachIndexed{ seasonIndex, seasonDirectory ->
-                            println(" | |-> ${seasonDirectory.title} (Key: ${seasonDirectory.ratingKey})")
-                            if(seasonDirectory.ratingKey != null){
-                                val seasonMetadata = plexApi.getSeasonMetadata(seasonDirectory.ratingKey!!.toInt())
-                                println(" | | * ${seasonMetadata.directories?.firstOrNull()?.summary}")
-                                val episodesMetadata = plexApi.getSeasonEpisodes(seasonDirectory.ratingKey!!.toInt())
-                                episodesMetadata.videos?.forEachIndexed{ episodeIndex, episodeDirectory ->
-                                    println(" | | |-> ${episodeDirectory.title} (Key: ${episodeDirectory.ratingKey})")
-                                    val episodeMetadata = plexApi.getEpisodeMetadata(episodeDirectory.ratingKey!!.toInt())
-                                    println(" | | | * ${episodeMetadata.videos?.firstOrNull()?.summary}")
+                    if (printShows) {
+                        println("## Series")
+                        val series = plexApi.getSeries(libraryDirectory.key!!.toInt())
+                        series.directory.forEachIndexed { seriesIndex, seriesDirectory ->
+                            println(" | Series $seriesIndex -> ${seriesDirectory.title} (Key: ${seriesDirectory.ratingKey})")
+                            val seriesMetadata = plexApi.getSeriesMetadata(seriesDirectory.ratingKey!!.toInt())
+                            println(" | * ${seriesMetadata.directories?.firstOrNull()?.summary}")
+                            val seasonsMetadata = plexApi.getSeriesSeasons(seriesDirectory.ratingKey!!.toInt())
+                            seasonsMetadata.directories?.forEachIndexed { seasonIndex, seasonDirectory ->
+                                println(" | |-> ${seasonDirectory.title} (Key: ${seasonDirectory.ratingKey})")
+                                if (seasonDirectory.ratingKey != null) {
+                                    val seasonMetadata = plexApi.getSeasonMetadata(seasonDirectory.ratingKey!!.toInt())
+                                    println(" | | * ${seasonMetadata.directories?.firstOrNull()?.summary}")
+                                    val episodesMetadata =
+                                        plexApi.getSeasonEpisodes(seasonDirectory.ratingKey!!.toInt())
+                                    episodesMetadata.videos?.forEachIndexed { episodeIndex, episodeDirectory ->
+                                        println(" | | |-> ${episodeDirectory.title} (Key: ${episodeDirectory.ratingKey})")
+                                        val episodeMetadata =
+                                            plexApi.getEpisodeMetadata(episodeDirectory.ratingKey!!.toInt())
+                                        println(" | | | * ${episodeMetadata.videos?.firstOrNull()?.summary}")
+                                    }
                                 }
                             }
                         }
@@ -58,25 +68,22 @@ fun main() {
                 }
 
                 "artist" -> {
-                    println("## Artists")
-                    val artists = plexApi.getArtists(libraryDirectory.key!!.toInt())
-                    artists.directory.forEachIndexed { artistIndex, artistDirectory ->
-                        println(" * Artist $artistIndex -> ${artistDirectory.title} (Key: ${artistDirectory.ratingKey})")
-                        val albumsMetadata = plexApi.getArtistAlbums(libraryDirectory.key, artistDirectory.ratingKey!!.toInt())
-                        albumsMetadata.directories?.forEachIndexed{ albumIndex, albumDirectory ->
-                            println(" | -> ${albumDirectory.title} (Key: ${albumDirectory.ratingKey})")
-                            val tracksMetadata = plexApi.getAlbumTracks(albumDirectory.ratingKey!!.toInt())
-                            tracksMetadata.tracks?.forEachIndexed{ trackIndex, trackDirectory ->
-                                println(" | | -> ${trackDirectory.title} (Key: ${trackDirectory.ratingKey})")
+                    if (printMusic) {
+                        println("## Artists")
+                        val artists = plexApi.getArtists(libraryDirectory.key!!.toInt())
+                        artists.directory.forEachIndexed { artistIndex, artistDirectory ->
+                            println(" * Artist $artistIndex -> ${artistDirectory.title} (Key: ${artistDirectory.ratingKey})")
+                            val albumsMetadata =
+                                plexApi.getArtistAlbums(libraryDirectory.key, artistDirectory.ratingKey!!.toInt())
+                            albumsMetadata.directories?.forEachIndexed { albumIndex, albumDirectory ->
+                                println(" | -> ${albumDirectory.title} (Key: ${albumDirectory.ratingKey})")
+                                val tracksMetadata = plexApi.getAlbumTracks(albumDirectory.ratingKey!!.toInt())
+                                tracksMetadata.tracks?.forEachIndexed { trackIndex, trackDirectory ->
+                                    println(" | | -> ${trackDirectory.title} (Key: ${trackDirectory.ratingKey})")
+                                }
                             }
                         }
                     }
-
-                    //println("## Albums")
-                    //val albums = plexApi.getAlbums(libraryDirectory.key.toInt())
-                    //albums.directory.forEachIndexed { albumIndex, albumDirectory ->
-                    //    println(" * Album $albumIndex -> ${albumDirectory.title} (Key: ${albumDirectory.ratingKey})")
-                    //}
                 }
             }
         }
